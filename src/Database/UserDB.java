d6af7a38be139c0434c202dc34cfda1ef21913e3
Đỗ Đức Khải <duckhaidev@gmail.com>
2021-12-23 09:32:28 +0700

    check login

--- src/Database/UserDB.java
+++ src/Database/UserDB.java
@@ -2,8 +2,29 @@ package Database;
 
 import Entity.User;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class UserDB {
-    public boolean checkUser(User user) {
+    public static boolean checkUser(User user) {
+        Connection conn = Connect.getConnect();
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM [USER]");
+
+            while (rs.next()) {
+                if (user.getUsername().equals(rs.getString("USERNAME")) && user.getPassword().equals(rs.getString("PASSWORD"))) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            Connect.closeConnect(conn);
+        }
+
         return false;
     }
 }
