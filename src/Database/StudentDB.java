c62a720d05accbe8f20561cfabdb975a7719f26a
Đỗ Đức Khải <duckhaidev@gmail.com>
2021-12-23 09:32:47 +0700

    Update addstudent

--- src/Database/StudentDB.java
+++ src/Database/StudentDB.java
@@ -1,4 +1,30 @@
 package Database;
 
+import Entity.Student;
+import View.Form.FormQLSV;
+
+import java.sql.*;
+
 public class StudentDB {
+    private static Connection conn;
+    private static Statement stmt;
+    private static ResultSet rs;
+    private static PreparedStatement preStmt;
+
+    public static void addStudent(Student student) {
+        conn = Connect.getConnect();
+        try {
+            preStmt = conn.prepareStatement("INSERT INTO STUDENT VALUES(?, ?, ?, ?, ?)");
+            preStmt.setString(1, student.getId());
+            preStmt.setString(2, student.getName());
+            preStmt.setByte(3, student.getAge());
+            preStmt.setString(4, student.getAddress());
+            preStmt.setDouble(5, student.getGPA());
+            preStmt.executeUpdate();
+
+            new FormQLSV().updateTable();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
